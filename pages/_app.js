164867bc30ee63711e21327f345e39ea2eb90a82
billgufran <billgufran@gmail.com>
2020-11-09 17:54:14 +0800

    Include Ant Design

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import App from "next/app";
+import React from "react";
+import "../styles/antd.less";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return <Component {...pageProps} />;
+  }
 }
 
-export default MyApp
+export default MyApp;
