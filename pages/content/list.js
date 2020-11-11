c2b87a76aa001fe3af316f980347052eee5d3cb1
billgufran <billgufran@gmail.com>
2020-11-12 01:00:09 +0800

    Add new item button

--- pages/content/list.js
+++ pages/content/list.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import ContentTables from '../../components/contentTables'
-import Sidebar from '../../components/sidebar'
+import { PlusOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+import Link from "next/link";
+import ContentTables from "../../components/ContentTables";
+import Sidebar from "../../components/SidebarLayout";
+
+//path: /content/list
 
 export default function ContentList() {
-   return (
-      <>
-         <Sidebar>
-            <ContentTables/>
-         </Sidebar>
-      </>
-   )
+	return (
+		<>
+			<Sidebar>
+				<ContentTables />
+				<Link href="/content/create">
+					<Button
+						type="primary"
+						shape="round"
+						icon={<PlusOutlined />}
+						size="large"
+					>
+						New Item
+					</Button>
+				</Link>
+			</Sidebar>
+		</>
+	);
 }
