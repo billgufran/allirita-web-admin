311f9e92181b60b097f178af9cf318857914e67d
billgufran <billgufran@gmail.com>
2020-11-12 01:00:22 +0800

    Add data provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
 import App from "next/app";
 import React from "react";
+import DataProvider from "../components/DataContext";
 import "../styles/antd.less";
 
-
 class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
+	render() {
+		const {Component, pageProps} = this.props;
 
-    return <Component {...pageProps} />;
-  }
+		return (
+			<DataProvider>
+				<Component {...pageProps} />
+			</DataProvider>
+		);
+	}
 }
 
 export default MyApp;
