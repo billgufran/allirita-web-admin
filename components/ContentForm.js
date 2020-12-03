1ac5a86c3b628093c519bde70fe9b62b787d6d22
billgufran <billgufran@gmail.com>
2020-12-03 20:30:44 +0800

    Modify error messages

--- components/ContentForm.js
+++ components/ContentForm.js
@@ -173,6 +173,39 @@ export default function ContentForm({
 		max: 255,
 	};
 
+	const defaultValidateMessages = {
+		default: "Validation error on field ${label}",
+		required: "${label} is required",
+		enum: "${label} must be one of [${enum}]",
+		whitespace: "${label} cannot be empty",
+		date: {
+		  format: "${label} is invalid for format date",
+		  parse: "${label} could not be parsed as date",
+		  invalid: "${label} is invalid date",
+		},
+		string: {
+		  len: "${label} must be exactly ${len} characters",
+		  min: "${label} must be at least ${min} characters",
+		  max: "${label} cannot be longer than ${max} characters",
+		  range: "${label} must be between ${min} and ${max} characters",
+		},
+		number: {
+		  len: "${label} must equal ${len}",
+		  min: "${label} cannot be less than ${min}",
+		  max: "${label} cannot be greater than ${max}",
+		  range: "${label} must be between ${min} and ${max}",
+		},
+		array: {
+		  len: "${label} must be exactly ${len} in length",
+		  min: "${label} cannot be less than ${min} in length",
+		  max: "${label} cannot be greater than ${max} in length",
+		  range: "${label} must be between ${min} and ${max} in length",
+		},
+		pattern: {
+		  mismatch: "${label} does not match pattern ${pattern}",
+		},
+	}
+
 	// === Upload button
 	const uploadProps = {
 		fileList,
@@ -245,6 +278,7 @@ export default function ContentForm({
 						layout="vertical"
 						name="content-form"
 						onFinish={onSubmit}
+						validateMessages={defaultValidateMessages}
 					>
 						<Form.Item
 							label="Title"
@@ -264,7 +298,7 @@ export default function ContentForm({
 							rules={[
 								{
 									...stringRules,
-									message: "Please fill content's description",
+									// message: "Please fill content's description",
 								},
 							]}
 						>
@@ -290,7 +324,7 @@ export default function ContentForm({
 							</Select>
 						</Form.Item>
 						<Form.Item
-							label="Video URL"
+							label="Video YouTube ID"
 							name="video"
 							rules={[
 								{
