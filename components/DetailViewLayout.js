947a96d0cc1b0c61db5b7091ec7c069c0717b1e1
billgufran <billgufran@gmail.com>
2020-12-04 01:45:37 +0800

    Change detail view layout to carousel

--- components/DetailViewLayout.js
+++ components/DetailViewLayout.js
@@ -1,6 +1,7 @@
-import { Button, Card } from "antd";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import { Button, Card, Carousel } from "antd";
 import Form from "antd/lib/form";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import api from "../services/api";
 import { AuthContext } from "./AuthContext";
 import ContentForm from "./ContentForm";
@@ -8,13 +9,15 @@ import QuizTable from "./QuizTable";
 import Sidebar from "./SidebarLayout";
 
 export default function DetailViewLayout({id_konten}) {
+	const carouselRef = useRef();
+
 	const [contentForm] = Form.useForm();
 
 	const [quizzes, setQuizzes] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [disableQuiz, setDisableQuiz] = useState(false);
 
-	const [imageName, setImageName] = useState("")
+	const [imageName, setImageName] = useState("");
 
 	const {user} = useContext(AuthContext);
 
@@ -30,15 +33,14 @@ export default function DetailViewLayout({id_konten}) {
 			// set value for content form
 			const content = res.data.data.getKonten;
 
-			setImageName(content.image)
+			setImageName(content.image);
 
 			content["kategori"] = content.kategori.map(item => item.nama_kategori);
-			content["image"] = null
+			content["image"] = null;
 			contentForm.setFieldsValue(content);
 
-
-			console.log("GET CONTENT RESPONSE")
-			console.log(content)
+			console.log("GET CONTENT RESPONSE");
+			console.log(content);
 
 			// set data for quiz table
 			const quizData = res.data.data.pertanyaan.map((el, i) => ({
@@ -64,47 +66,42 @@ export default function DetailViewLayout({id_konten}) {
 		}
 	}, []);
 
+	const cardHeader = (
+		<>
+			<Button
+				type="text"
+				icon={<ArrowLeftOutlined />}
+				style={{marginRight: 8}}
+				onClick={() => carouselRef.current.prev()}
+			/>
+			Quiz details
+		</>
+	);
+
 	return (
 		<Sidebar select="1">
-			<Card
-				bordered={false}
-				style={{width: "100%"}}
-				bodyStyle={{
-					padding: 0,
-					display: "flex",
-					flexDirection: "column",
-				}}
-			>
+			<Carousel ref={carouselRef}>
+				{/* Content Card */}
 				<ContentForm
 					id_konten={id_konten}
 					contentForm={contentForm}
 					isLoading={isLoading}
 					imageName={imageName}
 					setDisableQuiz={setDisableQuiz}
+					carouselRef={carouselRef}
 				/>
-				{(!!id_konten && !disableQuiz) && (
-					<Card
-						title="Quiz details"
-						bordered={false}
-						style={{width: "100%"}}
-					>
-						<QuizTable
-							quizzes={quizzes}
-							isLoading={isLoading}
-							getSelectedContent={getSelectedContent}
-							id_konten={id_konten}
-						/>
-					</Card>
-				)}
 
-				<Button
-					onClick={() => contentForm.submit()}
-					type="primary"
-					style={{alignSelf: "flex-end", margin: 24}}
-				>
-					Save
-				</Button>
-			</Card>
+				{/* Quiz Card */}
+				<Card title={cardHeader} bordered={false} style={{width: "100%"}}>
+					<QuizTable
+						quizzes={quizzes}
+						isLoading={isLoading}
+						getSelectedContent={getSelectedContent}
+						id_konten={id_konten}
+						carouselRef={carouselRef}
+					/>
+				</Card>
+			</Carousel>
 		</Sidebar>
 	);
 }
